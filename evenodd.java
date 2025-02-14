d187bfbe54a80b1390ab8bd108561c7b2d5a6c1e
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-02-14 19:53:00 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+class evenodd
+{
+	public static void main(String arg[])
+	{
+	int a=10;
+	if(a%2==0)
+		System.out.println("number is even number");
+	else
+		System.out.println("number is odd number");
+
+	}
+}
+	
