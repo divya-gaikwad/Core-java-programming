929e6c01d8e282ef11a96df67c73c518c411551b
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-02-21 08:34:47 +0530

    Add files via upload

---
+++ square_areas.java
@@ -0,0 +1,10 @@
+class square_areas
+{
+	public static void main(String arg[])
+	{
+		double length,area;
+		length=20;
+		area=length*length;
+		System.out.println("The area of square is ="+area);
+}
+}
