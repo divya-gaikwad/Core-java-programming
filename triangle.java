06bc03b9258997d677af436547de7c1edd824991
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-02-21 08:37:52 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,11 @@
+class triangle
+{
+	public static void main(String arg[])
+	{
+		double base,height,area;
+		base=20;
+		height=30;
+		area=0.5*base*height;
+		System.out.println("The area of triangle is ="+area);
+}
+}
