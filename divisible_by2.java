c9e1ec88893ad4ff60ecb261a2b67ddd23149fd7
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-02-23 00:13:30 +0530

    Add files via upload

---
+++ divisible_by2.java
@@ -0,0 +1,13 @@
+class divisible_by2
+{
+	public static void main(String arg[])
+	{
+	int a=10;
+	if(a%2==0)
+		System.out.println("number is divisible by 2");
+	else
+		System.out.println("number not is divisible by 2");
+
+	}
+}
+	
