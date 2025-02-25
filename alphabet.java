246908f06f30cc2f912f84dc3dcc35fa6effaad2
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-02-25 23:19:06 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,12 @@
+class alphabet
+{
+	public static void main(String arg[])
+	{
+	char ch='4';
+	if(((ch>='A')&&(ch<='Z'))||((ch>='a')&&(ch<='z')))
+	System.out.println("Entered charachter is alphabet");
+else
+	System.out.println("Entered charachter is not alphabet");
+	
+	}
+}
