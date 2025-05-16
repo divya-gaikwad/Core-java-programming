5a83de256352c28a62b427006ac6c166b713bbba
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-05-16 19:24:00 +0530

    Add files via upload

---
+++ stati.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+ class stati
+ {
+ 	static int a;
+ 	
+	static void show()
+	{
+    int b;
+	System.out.println("volume of sphere="+a);
+	System.out.println("volume of sphere="+b);
+	}
+	public static void main(String arg[])
+	{
+	stati ob=new stati();
+	ob.show();
+	stati.show();
+	show();
+	}
+}
