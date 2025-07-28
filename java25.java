7241c756c5681430b7e0334acc6f26f4dba231dc
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-07-28 11:20:45 +0530

    Add files via upload

---
+++ java25.java
@@ -0,0 +1,11 @@
+class java25
+{
+public static void main(String []arg)
+{
+System.out.println("--------------STUDENT RECORD -------------");
+System.out.println("Student Name : Divya Ravindra Gaiwkad");
+System.out.println("Student id :123435465");
+
+
+}
+}
