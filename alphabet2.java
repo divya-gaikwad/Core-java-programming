61d73126d47afd41e1fe09569c2aba373316e1f2
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-07-29 16:51:45 +0530

    Add files via upload

---
+++ alphabet2.java
@@ -0,0 +1,12 @@
+class alphabet2
+{
+public static void main(String arg[])
+{
+	char ch='d';
+	if ((ch>='A'  & ch<='Z')||(ch>='a' &&  ch<='z'))
+		System.out.println("Alphabet");
+	else
+		System.out.println("Not Alphabet");
+	
+}
+}
