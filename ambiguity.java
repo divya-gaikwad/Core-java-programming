52e6a685f53ea271cfa5e81e0c4d0a39f2c91d86
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-08-01 13:08:24 +0530

    Add files via upload

---
+++ ambiguity.java
@@ -0,0 +1,5 @@
+class ambiguity{void add(int x,int y){System.out.println("sum is ="+(x+y));}void add(int c,float d){System.out.println("sum is ="+(c+d));}void add(float e,int f){System.out.println("sum is ="+(e+f));}
+public static void main(String arg[]){	ambiguity ob=new ambiguity();	ob.add(10,20);	ob.add(10,30f);	ob.add(10f,400);}
+
+}
+
